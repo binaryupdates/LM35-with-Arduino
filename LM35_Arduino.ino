83542bff7aba26f3a6a670c08d7f1d3084bb6718
BINARYUPDATES.COM <binaryupdates@gmail.com>
2018-07-09 14:28:31 +0530

    Add files via upload

---
+++ LM35_Arduino.ino
@@ -0,0 +1,14 @@
+float temp = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  temp = analogRead(A0);
+  temp = temp * 0.48828125;
+  Serial.print("TEMPERATURE: ");
+  Serial.print(temp);
+  Serial.println(" *C");
+  delay(1000);
+}
